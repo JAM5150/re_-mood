738dd82b925176f3a901cd766b71c85d8525168d
beamnow <cd2667514@gmail.com>
2022-07-19 01:24:19 +0900

    update

--- Web/blog/js/playlistEdit.js
+++ Web/blog/js/playlistEdit.js
@@ -10,9 +10,28 @@ $("#saveBtn").on("click", function(){
         $("#saveForm").submit();
 }); */}
 
+//삭제되어야 하는 값을 체크 후 삭제버튼 눌렀을 때 실행되는 함수
+function getCheckboxValue() {
+    //선택된 목록 가져오기
+    const query = 'input[name="chklist"]:checked';
+    const selectedEls =document.querySelectorAll(query);
 
-$(function(){
-    $('.del').click(function(){
-       location.replace('');
-    });
-});
+      // 선택된 목록에서 value 찾기 chklist중 선택된 value값 result에 저장
+  let result = '';
+  selectedEls.forEach((el) => {
+    result += el.value + ' ';
+  });
+  
+  // html 상에서 출력 > html에 객체 선언안돼서 주석해제 해도 안댐
+  //document.getElementById('result').innerText = result;
+
+  console.log(JSON.parse(JSON.stringify(result)));
+
+}
+
+
+// $(function(){
+//     $('.del').click(function(){
+//        location.replace('');
+//     });
+// });
