872a9d855d7c9c2a193464b78f04c71bee9d12de
beamnow <cd2667514@gmail.com>
2022-07-19 14:53:09 +0900

    update

--- Web/blog/js/playlistJoyEdit.js
+++ Web/blog/js/playlistJoyEdit.js
@@ -11,31 +11,23 @@ $("#saveBtn").on("click", function(){
 }); */}
 // $('input[name=chklist]:checked').each(function() {	$(this).toggleClass("checksign", true); // addClass, removeClass   });
 
+
 //삭제되어야 하는 값을 체크 후 삭제버튼 눌렀을 때 실행되는 함수
 function getCheckboxValue() {
-    //선택된 목록 가져오기
-    const query = 'input[name="chklist"]:checked';
-    const selectedEls =document.querySelectorAll(query);
+  //선택된 목록 가져오기 (체크박스에 체크된 것=삭제할 목록)
+  const query = 'input[name="chklist"]:checked';
+  const selectedEls =document.querySelectorAll(query);
 
-      // 선택된 목록에서 value 찾기 chklist중 선택된 value값 result에 저장
+  // 선택된 목록에서 value 찾기 chklist중 선택된 value값 result에 저장
   let result = '';
   selectedEls.forEach((el) => {
     result += el.value + ' ';
   });
-  
-  // html 상에서 출력 > html에 객체 선언안돼서 주석해제 해도 안댐
-  //document.getElementById('result').innerText = result;
 
   //콘솔로 확인 > 출력은 music0 music2 이런 식
   console.log(JSON.parse(JSON.stringify(result)));
 
-
+  //기선택했던 플리로 이동 >replace사용으로 뒤로가기 눌렀을 때 편집화면으로 못 가도록함
+  location.replace('playlistJoy.html')
 
 }
-
-
-// $(function(){
-//     $('.del').click(function(){
-//        location.replace('');
-//     });
-// });
