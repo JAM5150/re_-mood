3234bd36ff4b361ba5dd39b123b57e3654c9f56f
JAM5150 <101987517+JAM5150@users.noreply.github.com>
2022-09-20 14:22:35 +0900

    Update config.php

--- config.php
+++ config.php
@@ -9,10 +9,10 @@ require_once 'google-api/vendor/autoload.php';
 $google_client = new Google_Client();
 
 //Set the OAuth 2.0 Client ID
-$google_client->setClientId('265344559907-eek9mfsqg5vanj0obpb581bodd4g7h4h.apps.googleusercontent.com');
+$google_client->setClientId('');
 
 //Set the OAuth 2.0 Client Secret key
-$google_client->setClientSecret('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');
+$google_client->setClientSecret('');
 
 //Set the OAuth 2.0 Redirect URI
 $google_client->setRedirectUri('http://moodprojecttest.tk/webtest/index.php');
